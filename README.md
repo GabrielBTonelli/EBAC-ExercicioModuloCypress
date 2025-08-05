128da2663ad4096730b62a79b563825f25755a1b
GabrielBTonelli <tonelli.gabriel@gmail.com>
2025-08-05 19:59:33 +0200

    Exercicio Gabriel

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # EBAC-ExercicioModuloCypress
+
+Pré-requisito:
+-Node.js - Você encontra em: https://nodejs.org/en/
+
+-Visual Studio Code ou qualquer editor de texto - você encontra em: https://code.visualstudio.com/download
+
+-Git: você encontra em: https://git-scm.com/downloads
+
+Para instalar as dependencias:
+npm install 
+Para executar em moodo Headlesss via console:
+npx cypress run
+Para executar via Dashboard:
+npx cypress open 
+Após abrir o dashboard, selecione um dos navegadores (De preferencia Electron) e siga com as execuções.
+
+Bibliotecas de apoio:
+-Cypress: Framework de automação: https://cypress.io/
--- cypress/e2e/exercicio-e2e.cy.js
+++ cypress/e2e/exercicio-e2e.cy.js
@@ -1,22 +1,63 @@
 /// <reference types="cypress" />
+const perfil = require('/Users/gabrieltonelli/repositorios/EBAC-ExercicioModuloCypress/cypress/fixtures/perfil.json')
+import paginaProdutos from '/Users/gabrieltonelli/repositorios/EBAC-ExercicioModuloCypress/cypress/support/page_objects/paginaProdutos.js.js'
 
-context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
-  /*  Como cliente 
-      Quero acessar a Loja EBAC 
-      Para fazer um pedido de 4 produtos 
-      Fazendo a escolha dos produtos
-      Adicionando ao carrinho
-      Preenchendo todas opções no checkout
-      E validando minha compra ao final */
+// context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
+//   /*  Como cliente 
+//       Quero acessar a Loja EBAC 
+//       Para fazer um pedido de 4 produtos 
+//       Fazendo a escolha dos produtos
+//       Adicionando ao carrinho
+//       Preenchendo todas opções no checkout
+//       E validando minha compra ao final */
+describe('Funcionalidade: Compra de produtos como cliente', () => {
+    
+    beforeEach(() => {
+        cy.visit('produtos')
+    });
+  
+    it('Deve realizar compra de 4 produtos', () => {
+        /// Fazendo login como cliente
+        cy.visit('minha-conta')
+        cy.get('#username').type(perfil.usuario)
+        cy.get('#password').type(perfil.senha)
+        cy.get('.woocommerce-form > .button').click()
 
-  beforeEach(() => {
-      cy.visit('/')
-  });
+        cy.get('.woocommerce-MyAccount-content > :nth-child(3)').should('exist')
 
-  it('Deve fazer um pedido na loja Ebac Shop de ponta a ponta', () => {
-      //TODO: Coloque todo o fluxo de teste aqui, considerando as boas práticas e otimizações
-      
-  });
+        /// Adicionando produtos específicos ao carrinho
+        cy.fixture('produtos').then(dados => {
+            paginaProdutos.buscarProduto(dados[0].nomeProduto)
+            paginaProdutos.addProdutoCarrinho(dados[0].tamanho, dados[0].cor, dados[0].quantidade)
+            cy.get('.woocommerce-message').should('contain', dados[0].nomeProduto)
+            cy.get('.dropdown-toggle > .mini-cart-items').should('contain', 1)
+    
+            paginaProdutos.buscarProduto(dados[1].nomeProduto)
+            paginaProdutos.addProdutoCarrinho(dados[1].tamanho, dados[1].cor, dados[1].quantidade)
+            cy.get('.woocommerce-message').should('contain', dados[1].nomeProduto)
+            cy.get('.dropdown-toggle > .mini-cart-items').should('contain', 2)
+    
+            paginaProdutos.buscarProduto(dados[2].nomeProduto)
+            paginaProdutos.addProdutoCarrinho(dados[2].tamanho, dados[2].cor, dados[2].quantidade)
+            cy.get('.woocommerce-message').should('contain', dados[2].nomeProduto)
+            cy.get('.dropdown-toggle > .mini-cart-items').should('contain', 3)
+    
+            paginaProdutos.buscarProduto(dados[3].nomeProduto)
+            paginaProdutos.addProdutoCarrinho(dados[3].tamanho, dados[3].cor, dados[3].quantidade)
+            cy.get('.woocommerce-message').should('contain', dados[3].nomeProduto)
+            cy.get('.dropdown-toggle > .mini-cart-items').should('contain', 4)
+    
+            ///Acessando o carrinho, fazendo checkout e validando compra
+            cy.get('.dropdown-toggle > .text-skin > .icon-basket').click()
+            cy.get('#cart > .dropdown-menu > .widget_shopping_cart_content > .mini_cart_content > .mini_cart_inner > .mcart-border > .buttons > .checkout').click()
+            cy.get('#order_comments').type('Pode entregar ao porteiro Márcio')
+            cy.get('#payment_method_cod').click()
+            cy.get('.wc_payment_method.payment_method_cod > .payment_box').should('contain', 'Pagar em dinheiro na entrega.')
+            cy.get('#terms').click()
+            cy.get('#place_order').click()
+            cy.wait(1000)
+            cy.get('.woocommerce-notice').should('contain', 'Obrigado. Seu pedido foi recebido.')
 
-
+        });
+    })
 })
